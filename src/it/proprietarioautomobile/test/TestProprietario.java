240e93b1b1eec51873974d8852483e81d10b814c
Matteo2104 <scarcella@outlook.it>
2022-02-26 16:29:20 +0100

    testate le funzioni get e remove Proprietario

--- src/it/proprietarioautomobile/test/TestProprietario.java
+++ src/it/proprietarioautomobile/test/TestProprietario.java
@@ -20,6 +20,10 @@ public class TestProprietario {
 			testListProprietario(proprietarioService);
 
 			testUpdateProprietario(proprietarioService);
+			
+			testGetProprietario(proprietarioService);
+			
+			testRemoveProprietario(proprietarioService);
 
 		} catch (Throwable e) {
 			e.printStackTrace();
@@ -70,5 +74,38 @@ public class TestProprietario {
 			throw new RuntimeException("non è stato possibile eseguire l'aggiornamento del record");
 
 		System.out.println("........ FINE testUpdateProprietario: successo ........");
+	}
+	
+	private static void testGetProprietario(ProprietarioService proprietarioService) throws Exception {
+		System.out.println("........ INIZIO testGetProprietario ........");
+		
+		Proprietario appenaInserito = new Proprietario("matteo", "scarcella");
+		proprietarioService.inserisciNuovo(appenaInserito);
+		if (appenaInserito.getId() == null)
+			throw new RuntimeException("non è stato possibile inserire un nuovo record");
+		
+		Proprietario trovatoConId = proprietarioService.caricaSingoloProprietario(appenaInserito.getId());
+		if (trovatoConId.getId() != appenaInserito.getId())
+			throw new RuntimeException("non è stato possibile caricare il record");
+
+		System.out.println("........ FINE testGetProprietario: successo ........");
+	}
+	
+	private static void testRemoveProprietario(ProprietarioService proprietarioService) throws Exception {
+		System.out.println("........ INIZIO testRemoveProprietario ........");
+		
+		Proprietario appenaInserito = new Proprietario("matteo", "scarcella");
+		proprietarioService.inserisciNuovo(appenaInserito);
+		if (appenaInserito.getId() == null)
+			throw new RuntimeException("non è stato possibile inserire un nuovo record");
+		
+		List<Proprietario> primaDellaRemove = proprietarioService.listAllProprietari();
+		proprietarioService.rimuovi(appenaInserito);
+		List<Proprietario> dopoLaRemove = proprietarioService.listAllProprietari();
+		
+		if (primaDellaRemove.size() == dopoLaRemove.size())
+			throw new RuntimeException("non è stato possibile eliminare il record");
+
+		System.out.println("........ FINE testRemoveProprietario: successo ........");
 	}
 }
